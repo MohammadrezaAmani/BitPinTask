ad10b38a7cbacc4e9eb4e19ab677969d2ba89285
MohammadrezaAmani <more.amani@yahoo.com>
2023-10-05 15:36:36 +0330

    code formating

--- api/views.py
+++ api/views.py
@@ -2,13 +2,16 @@ from rest_framework import generics, permissions
 from .models import Content, Rating
 from .serializers import ContentSerializer, RatingSerializer
 
+
 class ContentListView(generics.ListAPIView):
     queryset = Content.objects.all()
     serializer_class = ContentSerializer
 
+
 class ContentDetailView(generics.RetrieveAPIView):
     queryset = Content.objects.all()
     serializer_class = ContentSerializer
+
 
 class ContentRatingView(generics.CreateAPIView, generics.UpdateAPIView):
     queryset = Rating.objects.all()
@@ -17,5 +20,5 @@ class ContentRatingView(generics.CreateAPIView, generics.UpdateAPIView):
 
     def get_object(self):
         user = self.request.user
-        content_id = self.kwargs['content_id']
+        content_id = self.kwargs["content_id"]
         return Rating.objects.get(user=user, content_id=content_id)
