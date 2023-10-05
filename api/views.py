911c7354e53968988eda87bad979937bffb380d4
MohammadrezaAmani <more.amani@yahoo.com>
2023-10-05 17:40:33 +0330

    add pagination

--- api/views.py
+++ api/views.py
@@ -2,18 +2,19 @@ from rest_framework import generics
 from .models import Content, Rating
 from .serializers import ContentSerializer, RatingSerializer
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.pagination import PageNumberPagination
 
 
 class ContentListView(generics.ListAPIView):
     queryset = Content.objects.all()
     serializer_class = ContentSerializer
-
+    pagination_class = PageNumberPagination
 
 class ContentRatingView(generics.CreateAPIView, generics.UpdateAPIView):
     queryset = Rating.objects.all()
     serializer_class = RatingSerializer
     permission_classes = (IsAuthenticated,)
-
+    pagination_class = PageNumberPagination
     def get_object(self):
         user = self.request.user
         content_id = self.kwargs.get("pk")
