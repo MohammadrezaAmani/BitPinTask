64a624c4cc502477363263edf9519e8c8452415b
MohammadrezaAmani <more.amani@yahoo.com>
2023-10-05 15:33:25 +0330

    add content and rating models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Content(models.Model):
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+
+class Rating(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.ForeignKey(Content, on_delete=models.CASCADE)
+    score = models.DecimalField(max_digits=3, decimal_places=1)
